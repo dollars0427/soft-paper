429f76b122cb4a4e5e9c69239c865d9b8d248ebd
Sardo Ip <sardo@pixelactionstudio.com>
2018-07-03 17:54:01 +0800

    If node is image and page overflowed, move it to next page

--- soft-paper.js
+++ soft-paper.js
@@ -1,8 +1,10 @@
 const Paper = {
+  _currentPage: null,
   split(content){
     const page = document.querySelector(content);
     const contentList = page.cloneNode(true).childNodes;
     page.innerHTML = ''; //Clear Page Content
+    this._currentPage = page;
     this._splitContent(page, contentList);
   },
   _splitContent(page, nodeList){
@@ -10,10 +12,24 @@ const Paper = {
       if(node.textContent){
         const texts = node.textContent.split('');
         this._splitText(page, texts);
+      }else{
+        //If node is image, or other type of node, insert it to current page or new page
+        const page = this._currentPage;
+        const pageParent = this._currentPage.parentNode;
+        page.appendChild(node);
+        if(this._isOverflowed(pageParent)){
+          page.innerHTML = page.innerHTML.slice(0, -1);
+          const newParent = pageParent.cloneNode(false);
+          const newPage = page.cloneNode(false);
+          pageParent.parentNode.appendChild(newParent);
+          newParent.appendChild(newPage);
+          newPage.appendChild(node);
+        }
       }
     });
   },
   _splitText(page, texts){
+    this._currentPage = page;
     const pageParent = page.parentNode;
     const fullTexts = texts.slice(0);
     for(let i = 0; i < texts.length; i++){
@@ -47,4 +63,3 @@ if (typeof window !== 'undefined') {
 }else{
   module.exports = Paper;
 }
-
